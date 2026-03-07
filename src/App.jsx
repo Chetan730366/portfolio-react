c976a79f9b31f684b61fc525765eddfffc56a37b
Chetan730366 <chetan730366@gmail.com>
2026-03-08 01:51:24 +0530

    fix: case sensitive import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Nav from "./components/Nav/Nav"
-import Home from "./components/home/Home"
+// import Home from "./components/Home/Home"
+
 import About from "./components/About/About"
 import Card from "./components/Card/Card"
+import Home from "./components/home/Home"
 function App() {
   return (
     <>
