0b99d5fd8e07457b7dab561c3861b65c88c9f2d0
Chetan730366 <chetan730366@gmail.com>
2026-03-08 18:51:23 +0530

    fix: correct Home import casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import Nav from "./components/Nav/Nav"
-// import Home from "./components/Home/Home"
-
 import About from "./components/About/About"
 import Card from "./components/Card/Card"
-import Home from "./components/home/Home"
+import Home from "./components/Home/Home"
+
 function App() {
   return (
     <>
