8b5f5c95021d6b9cd383f997e823ac5f4e46b7f5
Chetan730366 <chetan730366@gmail.com>
2026-03-08 15:51:56 +0530

    fix: empty src in Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,11 @@
 import React from 'react'
 import "./Card.css"
-import { Typewriter } from "react-simple-typewriter";
+
 
 function Card() {
     return (
         <div id='about'>
-            <div className="leftabout">
-                <div className="about">
-                    <div className="aboutline1">hello !</div>
-                    <div className="aboutline2">I'm chetan</div>
-                    <div className="aboutline3">
-                        <Typewriter
-                            words={["Web Developer", "Front-End Developer"]}
-                            typeSpeed={100}
-                            delaySpeed={1500}
-                            loop={true}
-                        />
-                    </div>
-                    <img src="" alt="" />
-                </div>details
-            </div>
-
+           
         </div>
     )
 }
